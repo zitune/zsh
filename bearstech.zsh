0f17e522d8bdd4f12447b8b86d5545d6189316c3
maudin <maudin@bearstech.com>
2017-05-24 17:34:36 +0200

    add nova-env for ovh cloud management

--- bearstech.zsh
+++ bearstech.zsh
@@ -3,7 +3,7 @@ export INFRA=/home/hybris/dev/bearstech/bearstech/infra
 export PATH=$INFRA/bin:$INFRA/../xen:$PATH
 
 # Everyday aliases
-bthost()	 {cat $INFRA/host/$1}
+bthost()   {cat $INFRA/host/$1}
 dom0()     {awk '/Container:/{ print $2}' ${INFRA}/host/${1}}
 alerts()   {ssh root@pidou monshow | grep -v "none$" | grep -v "^     " | grep -v "^$"}
 tickets()  {curl "https://maudin:$(/home/hybris/.scripts/keepcli -p Dashbeard)@dashbeard.bearstech.com/api/dashbeard.txt"}
@@ -14,3 +14,6 @@ vpn-bear() {for i in $(nmcli con | grep vpn | grep -v -- -- | cut -d' ' -f1); do
 source '/home/hybris/dev/bearstech/google-cloud-sdk/path.zsh.inc'
 # The next line enables shell command completion for gcloud.
 source '/home/hybris/dev/bearstech/google-cloud-sdk/completion.zsh.inc'
+
+# OVH cloud
+nova-env ()     {source $INFRA/cloud/ovh-$1.env}
