8414b77fc5425b7861d98840d7ec3d75a855fd39
maudin <maudin@bearstech.com>
2017-06-01 10:58:58 +0200

    prefix for every work aliases, sort

--- bearstech.zsh
+++ bearstech.zsh
@@ -3,12 +3,12 @@ export INFRA=/home/hybris/dev/bearstech/bearstech/infra
 export PATH=$INFRA/bin:$INFRA/../xen:$PATH
 
 # Everyday aliases
-bthost()   {cat $INFRA/host/$1}
-bteach()   {$INFRA/bin/foreach $@}
-dom0()     {awk '/Container:/{ print $2}' ${INFRA}/host/${1}}
-alerts()   {ssh root@pidou monshow | grep -v "none$" | grep -v "^     " | grep -v "^$"}
-tickets()  {curl "https://maudin:$(/home/hybris/.scripts/keepcli -p Dashbeard)@dashbeard.bearstech.com/api/dashbeard.txt"}
-vpn-bear() {for i in $(nmcli con | grep vpn | grep -v -- -- | cut -d' ' -f1); do nmcli con down $i; done; nmcli con up bearstech}
+bt-alerts()     {ssh root@pidou monshow | grep -v "none$" | grep -v "^     " | grep -v "^$"}
+bt-dom0()       {awk '/Container:/{ print $2}' ${INFRA}/host/${1}}
+bt-each()       {$INFRA/bin/foreach $@}
+bt-host()       {cat $INFRA/host/$1}
+bt-tickets()    {curl "https://maudin:$(/home/hybris/.scripts/keepcli -p Dashbeard)@dashbeard.bearstech.com/api/dashbeard.txt"}
+bt-vpn()        {for i in $(nmcli con | grep vpn | grep -v -- -- | cut -d' ' -f1); do nmcli con down $i; done; nmcli con up bearstech}
 
 # Google Cloud
 # The next line updates PATH for the Google Cloud SDK.
