501cfd91153d73acd734cea9fee1dbb0941da42d
Zitune <oandre@bearstech.com>
2019-02-15 18:20:52 +0100

    update bearstech file

--- bearstech.zsh
+++ bearstech.zsh
@@ -3,12 +3,13 @@ export INFRA=/home/zitune/dev/bearstech/infra
 export PATH=$INFRA/bin:$PATH
 
 # Everyday aliases
-bt-alerts()     {ssh root@pidou monshow | grep -v "none$" | grep -v "^     " | grep -v "^$"}
-bt-dom0()       {awk '/Container:/{ print $2}' ${INFRA}/host/${1}}
-bt-each()       {$INFRA/bin/foreach $@}
-bt-host()       {cat $INFRA/host/$1}
-bt-release()    {ssh root@$1 cat /etc/debian_version}
-bt-tickets()    {echo "Tickets:$(curl -s "https://maudin:$(/home/zitune/.scripts/keepcli -p Dashbeard)@dashbeard.bearstech.com/api/dashbeard.txt")"}
+monshow()    {ssh root@pidou monshow | grep -v "none$" | grep -v "^     " | grep -v "^$"}
+dom0()       {awk '/Container:/{ print $2}' ${INFRA}/host/${1}}
+beach()      {$INFRA/bin/foreach $@}
+bhost()      {cat $INFRA/host/$1}
+bdebian()    {ssh root@$1 cat /etc/debian_version}
+btickets()   {echo "Tickets:$(curl -s "https://maudin:$(/home/zitune/.scripts/keepcli -p Dashbeard)@dashbeard.bearstech.com/api/dashbeard.txt")"}
+bssh()       {ssh -A root@$1}
 
 # Google Cloud
 # The next line updates PATH for the Google Cloud SDK.
@@ -16,13 +17,22 @@ bt-tickets()    {echo "Tickets:$(curl -s "https://maudin:$(/home/zitune/.scripts
 # The next line enables shell command completion for gcloud.
 #source '/home/zitune/dev/bearstech/google-cloud-sdk/completion.zsh.inc'
 
-# OVH cloud
-nova-env()            {source $INFRA/cloud/ovh-$1.env}
+hh() {
+    while (($#))
+  do
+	echo "$1 :"
+	host ${${1##https#://}%%/*} | sed -n -e /'.*'/P -e s@"^.* has address \(.*\)$"@"host \1"@ep
+	echo
+	shift
+    done
+}
+
 
 # Some completion functions
-_complete_bt-host()   {reply=($(ls $INFRA/host))}
+_complete_bhost()   {reply=($(ls $INFRA/host))}
 _complete_nova-env()  {reply=($(ls $INFRA/cloud/ovh-*.env | sed "s@$INFRA/cloud/ovh-\(.*\).env@\1@"))}
-compctl -K _complete_bt-host  bt-dom0
-compctl -K _complete_bt-host  bt-host
-compctl -K _complete_bt-host  bt-release
-compctl -K _complete_nova-env nova-env
+compctl -K _complete_bhost  dom0
+compctl -K _complete_bhost  bhost
+compctl -K _complete_bhost  bdebian
+compctl -K _complete_bhost  bssh
+
