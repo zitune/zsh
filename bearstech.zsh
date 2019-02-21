1ba0f71602f047cc4b53ad58b2ee741cd7fb30ae
Zitune <oandre@bearstech.com>
2019-02-21 12:06:15 +0100

    update

--- bearstech.zsh
+++ bearstech.zsh
@@ -10,6 +10,10 @@ bhost()      {cat $INFRA/host/$1}
 bdebian()    {ssh root@$1 cat /etc/debian_version}
 btickets()   {echo "Tickets:$(curl -s "https://maudin:$(/home/zitune/.scripts/keepcli -p Dashbeard)@dashbeard.bearstech.com/api/dashbeard.txt")"}
 bssh()       {ssh -A root@$1}
+r()          {ssh -A root@$1}
+
+#aliases
+alias monshow='ssh root@pidou monshow'
 
 # Google Cloud
 # The next line updates PATH for the Google Cloud SDK.
@@ -30,9 +34,11 @@ hh() {
 
 # Some completion functions
 _complete_bhost()   {reply=($(ls $INFRA/host))}
+_complete_allhost()   {reply=($(ls $INFRA/host $ZINFRA/host))}
 _complete_nova-env()  {reply=($(ls $INFRA/cloud/ovh-*.env | sed "s@$INFRA/cloud/ovh-\(.*\).env@\1@"))}
 compctl -K _complete_bhost  dom0
 compctl -K _complete_bhost  bhost
-compctl -K _complete_bhost  bdebian
-compctl -K _complete_bhost  bssh
+compctl -K _complete_allhost  bdebian
+compctl -K _complete_allhost  bssh
+compctl -K _complete_allhost  r
 
--- zitune.zsh
+++ zitune.zsh
@@ -4,6 +4,10 @@ function precmd() {echo -n -e "\a"}
 # do not share history between terms...
 unsetopt share_history
 
+# ZINFRA like infra
+export ZINFRA=/home/zitune/dev/vexatil/infra
+export PATH=$ZINFRA/bin:$PATH
+
 # Load keychain
 # keychain -q --agents ssh id_rsa id_dsa
 
@@ -15,10 +19,37 @@ health()	{/usr/bin/screen -O -S health -c ~/.config/screen_health}
 j()		{if [ $# -eq 0 ]; then marks; else jump $@; fi}
 loop()		{while [ 1 ]; do sh -c "$@"; done}
 open_ports()	{sudo netstat -tulpen 2> /dev/null | grep "LISTEN"}
-r()		{if [ $# -eq 1 ]; then ssh root@$1; else sudo su -; fi}
 dontknow()      {echo -n "¯\\_(ツ)_/¯" | xclip}
 def()           {echo "default_path = '$PWD'" | awesome-client}
 dchroot()       {docker run --rm --privileged -ti $@ debian:stretch /bin/bash}
+
+
+
+#historic
+HISTSIZE=1000
+SAVEHIST=5000
+HISTFILE=~/.zshhistory
+
+alias ltr='ls -ltr'
+alias go='ssh-add'
+alias t='urxvt &!'
+
+#alias
+alias xterm='xterm -bg black -fg white'
+alias vi='vim'
+
+alias rl='ssh -A root@localhost'
+alias rscp='scp -o User=root'
+
+alias offl='killall offlineimap;rm .offlineimap/*.lock; offlineimap'
+alias asmo='firejail --name=asmo --profile=/home/zitune/.config/firejail/asmo.profile google-chrome  --user-data-dir=/home/zitune/.config/asmo --cipher-suite-blacklist=0x0033,0x0039,0x009E,0xcc15'
+
+
+alias controller1="sudo  xboxdrv  --silent --detach-kernel-driver -d  --mimic-xpad --led 1"
+alias controller2="sudo  xboxdrv  --silent --detach-kernel-driver -d  --mimic-xpad --led 2"
+
+
+
 
 # Usefull bindings
 bindkey "^[Od" backward-word
